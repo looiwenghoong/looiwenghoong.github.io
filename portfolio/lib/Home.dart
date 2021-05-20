827be9ce2b7862ada9f587b3e0660540f50ab7ff
Looi <https://github.com/looiwenghoong/WallCom.git>
2021-05-20 20:30:47 +0800

    feat: Fixed flutter web build for deployment #2

--- portfolio/lib/Home.dart
+++ portfolio/lib/Home.dart
@@ -9,8 +9,52 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello world in flutter"),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: Colors.black,
+            child: Text(
+              " Please don't click that button ",
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image(
+            image: AssetImage("images/spooky_bg.jpg"),
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Text(
+                      "Trick or Treat",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Image.network(
+                      "https://media.giphy.com/media/h2CfczI1ggcspHZ26z/giphy.gif"),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
